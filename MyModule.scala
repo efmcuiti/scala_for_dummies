23d4d153d225fdc7d93fe868ed2998c406ecc170
Edison Mendez <efmcuiti@gmail.com>
2019-05-19 20:00:50 -0500

    Introduce HOF (Higher Order Functions)

--- MyModule.scala
+++ MyModule.scala
@@ -8,6 +8,25 @@ object MyModule {
     msg.format(x, abs(x))
   }
 
+  def factorial(n: Int): Int = {
+    def go(n: Int, acc: Int): Int =
+      if (n <= 0) acc
+      else go(n-1, n*acc)
+
+    go(n,1)
+  }
+
+  def fib(n: Int): Int = {
+    if (n == 0) 0
+    else if (n == 1) 1
+    else fib(n-1) + fib(n-2)
+  }
+
+  def formatResult(name: String, n: Int, f: Int => Int) = {
+    val msg = "The %s of %d is %d."
+    msg.format(name, n, f(n))
+  }
+
   def main(args: Array[String]): Unit =
     println(formatAbs(-42))
 }
